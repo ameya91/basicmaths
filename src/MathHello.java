ad743326f917da01a1279d54dcb83fd1ab323891
Ameya <ameyadighe91@gmail.com>
2015-05-12 15:02:22 +0530

    first commit

---
+++ src/MathHello.java
@@ -0,0 +1,24 @@
+
+public class MathHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=10, num2=20;
+System.out.println("Addition is:");
+System.out.println(num1+num2);
+
+System.out.println("Subtraction is:");
+System.out.println(num2-num1);
+	
+System.out.println("Multiplication is:");
+System.out.println(num2*num1);
+
+System.out.println("Division is:");
+System.out.println(num2/num1);
+	
+System.out.println("Module is:");
+System.out.println(num2%num1);
+
+	}
+	}
+
